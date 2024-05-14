475fb15674d7b4547c76011280c11d2296f27231
Nobuto Murata <nobuto@ubuntu.com>
2024-05-14 23:24:48 +0900

    more FQDN

--- redeploy-microstack.sh
+++ redeploy-microstack.sh
@@ -38,9 +38,9 @@ for i in {1..3}; do
     uvt-kvm wait "sunbeam-${i}.localdomain"
 done
 
-uvt-kvm ssh sunbeam-1 -- -t sudo snap install openstack --channel 2024.1/edge
-uvt-kvm ssh sunbeam-1 -- tee deployment_manifest.yaml < manifest.yaml
-#uvt-kvm ssh sunbeam-1 -- 'tail -n+2 /snap/openstack/current/etc/manifests/edge.yml >> deployment_manifest.yaml'
-uvt-kvm ssh sunbeam-1 -- 'sunbeam prepare-node-script | bash -x'
-uvt-kvm ssh sunbeam-1 -- -t time sunbeam cluster bootstrap --manifest deployment_manifest.yaml --role control --role compute --role storage \
+uvt-kvm ssh sunbeam-1.localdomain -- -t sudo snap install openstack --channel 2024.1/edge
+uvt-kvm ssh sunbeam-1.localdomain -- tee deployment_manifest.yaml < manifest.yaml
+#uvt-kvm ssh sunbeam-1.localdomain -- 'tail -n+2 /snap/openstack/current/etc/manifests/edge.yml >> deployment_manifest.yaml'
+uvt-kvm ssh sunbeam-1.localdomain -- 'sunbeam prepare-node-script | bash -x'
+uvt-kvm ssh sunbeam-1.localdomain -- -t time sunbeam cluster bootstrap --manifest deployment_manifest.yaml --role control --role compute --role storage \
     || (juju model-default --cloud sunbeam-microk8s logging-config='<root>=INFO;unit=DEBUG'; juju model-config -m openstack logging-config='<root>=INFO;unit=DEBUG')  # LP: #2065490
