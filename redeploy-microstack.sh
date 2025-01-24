b30aedaa63b04116f546d2c53bf50132ee836e37
Nobuto Murata <nobuto@ubuntu.com>
2025-01-24 09:56:00 +0200

    let's clean up by hand for now.
    Jan 24 07:54:59 sunbeam-1 lxd.daemon[6398]: time="2025-01-24T07:54:57Z"
    level=warning msg="Dqlite: attempt 1: server 1: write handshake: write
    unix @->@a41a0: i/o timeout"
    
    Jan 24 07:54:59 sunbeam-1 lxd.daemon[6398]: time="2025-01-24T07:54:55Z"
    level=error msg="Failed getting custom volume info" err="Failed to begin
    transaction: failed to create dqlite connection: no available dqlite
    leader server found"
    
    Jan 24 07:55:05 sunbeam-1 lxd.daemon[6398]: time="2025-01-24T07:54:57Z"
    level=warning msg="Transaction timed out. Retrying once" err="Failed to
    begin transaction: context deadline exceeded" member=1

--- redeploy-microstack.sh
+++ redeploy-microstack.sh
@@ -82,6 +82,10 @@ ssh_to 1 -t -- \
     time sunbeam cluster bootstrap --manifest deployment_manifest.yaml \
         --role control,compute,storage
 
+# LP: #2095487
+ssh_to 1 -t -- \
+    time juju destroy-controller localhost-localhost --no-prompt
+
 # LP: #2065490
 #ssh_to 1 -- 'juju model-default --cloud sunbeam-microk8s logging-config="<root>=INFO;unit=DEBUG"'
 #ssh_to 1 -- 'juju model-config -m openstack logging-config="<root>=INFO;unit=DEBUG"'
